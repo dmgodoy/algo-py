55227f0a07808f04be4a13cd553a3fabd2d8384e
David Medina <davidmedinagodoy@gmail.com>
2024-07-14 20:06:54 +0200

    lomuto partioning for quicksort

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -1,23 +1,20 @@
 def partition(arr: list[int], l: int, r:int) -> None:
-    pivot = arr[l]
-    lo, hi = l + 1, r
-    while lo < hi:
-        while lo <= hi and arr[lo] < pivot: lo += 1 #strict comparison is important for cases with repeated elements
-        while lo <= hi and arr[hi] > pivot: hi -= 1 #idem
-        if lo < hi:
-            arr[lo], arr[hi] = arr[hi], arr[lo]
-    if arr[l] > arr[hi]:
-        arr[l], arr[hi] = arr[hi], arr[l]
-        return lo 
-    return hi 
+    pivot = arr[r]
+    pivot_index = l
+    for i in range(l,r):
+        if arr[i] <= pivot:
+            arr[i],arr[pivot_index]=arr[pivot_index],arr[i]
+            pivot_index += 1
+    arr[r],arr[pivot_index]=arr[pivot_index],arr[r]
+    return pivot_index
 def qs(arr: list[int], l: int, r:int) -> None:
     if l >= r: return
     p = partition(arr, l, r)
     qs(arr,l,p-1)
     qs(arr,p+1,r)
 
-arr = [3,4,1,2,7,8,3,5,7,9]
+arr = [4,1,3,2,7,8,9,6,6]#[3,4,1,2,7,8,3,5,7,9]
 print(arr)
-print(qs(arr, 0, len(arr)-1))
+qs(arr, 0, len(arr)-1)
 print(arr)
 
